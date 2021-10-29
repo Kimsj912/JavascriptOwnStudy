04b3c33a301a50fe609e25ea9a8b24587074533a
portfolio_1st <aa991122aa@gmail.com>
2021-10-30 00:29:55 +0900

    get form input & hidden  form & remove hidden from greeting message

--- momentum/app.js
+++ momentum/app.js
@@ -1,17 +1,21 @@
 // github으로 날짜에 맞춰 업로드된 거 따라가면됨. 
 // 이제 주석으로 안남기고 그냥 깃헙에 올린 뒤 지울거임.
 const loginForm = document.querySelector("#login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
-const link = document.querySelector("a");
+const loginInput = loginForm.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function onLoginSubmit(){
-}
-loginButton.addEventListener("submit",onLoginSubmit);
+const HIDDEN_CLASSNAME = "hidden";
 
-function handleLinkClick(event){
-    // alert("alert는 특이해서 실행시키면 페이지 넘어가려던것도 막아선다. 그리고 확인을 누르면 하려던 일을 마저한다.");
+function onLoginSubmit(event){
+    // initialize setting
     event.preventDefault();
-    console.dir(event);
+
+    // remove loginform
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    
+    // get username and  show greeting message
+    const username = loginInput.value;
+    greeting.innerText=`Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-link.addEventListener("click",handleLinkClick);
+loginForm.addEventListener("submit",onLoginSubmit);
