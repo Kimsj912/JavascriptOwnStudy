a8733e000eb3f4db92ca8cdf067b89f18f6049e9
portfolio_1st <aa991122aa@gmail.com>
2021-10-30 00:12:04 +0900

    Event학습 내용

--- momentum/app.js
+++ momentum/app.js
@@ -1,2 +1,19 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+const link = document.querySelector("a");
 
-console.log(5)
+function onLoginSubmit(){
+    // html을 이용해 막았음.
+    // const username = loginInput.value;
+    // if(username=="") alert("please write your name");
+    // else if(username.length>15) alert("your name is too long.");
+}
+loginButton.addEventListener("submit",onLoginSubmit);
+
+function handleLinkClick(event){
+    // alert("alert는 특이해서 실행시키면 페이지 넘어가려던것도 막아선다. 그리고 확인을 누르면 하려던 일을 마저한다.");
+    event.preventDefault();
+    console.dir(event);
+}
+link.addEventListener("click",handleLinkClick);
