db4781b007b1e054302bf9ea48368a475d6f5a9b
portfolio_1st <aa991122aa@gmail.com>
2021-11-07 01:20:00 +0900

    1. 체력 하트바 만듬 2. 게임시작 상태 모습 만듬 3. 게임종료상태 모습 만듬 4. 다시 시작 버튼 추가

--- TypingGame/js/main.js
+++ TypingGame/js/main.js
@@ -4,75 +4,147 @@ const GAME_RUNNING = "게임중";
 const GAME_END = "게임종료";
 const GAME_LOADING_KOR = "게임 로딩중..";
 const GAME_LOADIND_ENG = "loading";
+const INITIAL_WORDDISPLAY = "PRESS GAME START";
+const FINAL_WORDDISPLAY = "Game-End!";
 const  GAME_TIME = 5;
+const BLUE_HEART = "💙";
+const BLACK_HEART ="🖤";
+const HEART_COUNT =  5;
 
 // Attribute
 const wordInput = document.querySelector(".word-input");
 const wordDisplay = document.querySelector(".word-display");
 const scoreDisplay = document.querySelector(".score");
 const timeDisplay = document.querySelector(".time");
+const heartBox = document.querySelector(".heart-box");
+const heartBar = document.querySelector(".heart-bar");
 const button = document.querySelector(".button");
+const restartBtn = document.querySelector(".restartBtn");
 
 // Variable
 let words = [];
-let score = 0;
-let time = 9;
-let isPlaying= false;
-let timeInterval;
-let checkInterval;
+let correctNum = 0; // 맞은 개수
+let hearts = HEART_COUNT; // 남은 하트수
+let score = 0; // 실제 점수
+let time = 9; // 시간
+let isPlaying= false; // 게임 중인지 여부
+let timeInterval; // 남은 시간 인터벌
+let checkInterval; //  시간과 게임중 여부를 이용해 상태를 파악하는 checkStatus를 갱신해주는 인터벌
 
 // Initialize
 init();
 function init(){
+    // display a loading message & loading game
     buttonChange(GAME_LOADING_KOR);
     getWords();
-    wordInput.addEventListener('input', chechMatch);
+    wordInput.addEventListener('input', chechMatch); 
+    makeHeart(); // make initalize heart bar (처음엔 비어있기 때문)
 }
 
 // playing game
 function run(){
+    // 이미 실행중이면 누르지 실행이 안되도록 막는다.
     if(isPlaying) return;
+    // 실행 셋팅
     isPlaying = true;
-    time = GAME_TIME;
+    // set word display
+    wordDisplay.innerText = words[0];
+    // set word input
     wordInput.focus();
-    scoreDisplay.innerText = 0;
+    // set time
+    time = GAME_TIME;
     timeInterval = setInterval(countDown,1000);
+    // set score
+    scoreDisplay.innerText = 0;
+    // start check status interval
     checkInterval = setInterval(checkStatus,50);
+    // set button
     buttonChange(GAME_RUNNING);
+}
+
+function reinit(){
+    // reloading words
+    buttonChange(GAME_LOADING_KOR);
+    getWords();
+    // time
+    time = 0;
+    timeDisplay.innerText = time;
+    // score
+    score = 0;
+    scoreDisplay.innerText = score;
+    // heart 
+    heartBox.innerHTML="";
+    hearts = HEART_COUNT;
+    makeHeart();
 }
 
 // check status
 function checkStatus(){
-    if(!isPlaying && time === 0){
-        buttonChange(GAME_START);
-        clearInterval(checkInterval);
+    if (time===0){
+        if(!isPlaying){ // 게임을 시작할 수 있는 상태가 되었음을 의미.
+            buttonChange(GAME_START);
+            clearInterval(checkInterval);
+        }
+        if (hearts==1){ // 하트가 한개 남은채로 시간이 0이된 케이스
+            // minus heart
+            hearts--;
+            makeHeart();
+            // finalize time
+            clearInterval(timeInterval);
+            // finalize word display
+            wordDisplay.innerText = FINAL_WORDDISPLAY;
+            // finalize button
+            let value = GAME_END+score;
+            button.innerText = value;
+            // quit checkInterval
+            clearInterval(checkInterval);
+        } else{
+            // minus heart
+            hearts--;
+            makeHeart();
+            // re-setting time 
+            time = GAME_TIME;
+            clearInterval(timeInterval);
+            timeInterval = setInterval(countDown,1000);
+            // re-setting word display
+            wordDisplay.innerText = words[Math.floor( Math.random() * words.length)];
+            // re-setting wordInput 
+            wordInput.value = "";
+            wordInput.focus();
+        }
     }
 }
 
 // get words
 function getWords(){
-    // Make a request for a user with a given ID
-    axios.get('https://random-word-api.herokuapp.com/all')
+    // get word at random-word 
+    axios.get('https://random-word-api.herokuapp.com/word?number=10000')
     .then(function (response) {
-        // handle success
+        // filtering
         response.data.forEach((word)=>{if(word.length < 10)  words.push(word);})
+        // set start status
+        wordDisplay.innerText = INITIAL_WORDDISPLAY;
         buttonChange(GAME_START);
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
+    }) .catch(function (error) { console.log(error);})
 }
 
 // check matching
 function chechMatch () {
+    // if input value and display value are the same
     if(wordInput.value.toLowerCase() === wordDisplay.innerHTML.trim().toLowerCase()){
-        wordInput.value="";
-        if(!isPlaying) return;
+        // the input should be cleared
+        wordInput.value=""; 
+        // 게임중이 아니라면 input만 비우고 종료
+        if(!isPlaying) return; 
 
-        score++;
-        scoreDisplay.innerText = score;
+        // 게임중이라면 여러 값들을 조정해줘야한다.
+        // score control
+        correctNum++; // 실제 맞은개수 증가
+        score += time * correctNum * 10; // 점수 계산
+        scoreDisplay.innerText = score; // 점수 표시
+        // time control
         time = GAME_TIME;
+        // word display control
         const randomIdx = Math.floor( Math.random() * words.length);
         wordDisplay.innerText = words[randomIdx];
     }
@@ -87,11 +159,27 @@ function countDown(){
 
 // change button's text
 function buttonChange(text){
+    // 입력받은 값을 button에 표시해주고,
     button.innerText =  text;
+    // loading에 따라 css를 적용해줘야하기 때문에 classList에 상태에 따라추가를 해준다.
     text === GAME_START 
     ? button.classList.remove(GAME_LOADIND_ENG)
     : button.classList.add(GAME_LOADIND_ENG);
 }
 
+// make initial heart
+function makeHeart(){
+    let initialHeart = "";
+    for (let index = 0; index < hearts; index++) {
+        initialHeart += `<div class='heart-bar'>${BLUE_HEART}</div>`;
+    }
+    for (let index = 0; index < HEART_COUNT-hearts; index++) {
+        initialHeart += `<div class='heart-bar'>${BLACK_HEART}</div>`;
+    }
+    heartBox.innerHTML =initialHeart;
+}
+
 // Events
+wordInput.addEventListener('input', chechMatch);
 button.addEventListener("click",run);
+restartBtn.addEventListener("click",reinit);
