c9a19f4249b992843a050e8ee6f4e5a8046d0792
portfolio_1st <aa991122aa@gmail.com>
2021-11-14 00:38:48 +0900

    입장 페이지 프론트 제작

--- chat/app.js
+++ chat/app.js
@@ -1,13 +1,15 @@
 // 사용 npm
 // npm install express socket.io moment 
 // npm install -g nodemon
+// npm install ngrok
 
 // Modules
 const express = require("express"); // node express를 사용할 수 있게 함.
 const http = require("http"); // socket io를 받을 수 있게 하기위함. +우린 웹소켓이니까 http를 사용
 const path = require("path") // 노드제이에스의 기본적인 모듈, url만들기 편리해짐
-const socketIO = require("socket.io"); 
+const socketIO = require("socket.io"); // socket통신 모듈
 const moment = require("moment");
+const fs = require('fs');
 
 // Constant
 const PORT = process.env.PORT || 5000;
@@ -17,11 +19,14 @@ const app = express(); // 익스프레스 실행내용을 담음.
 const server = http.createServer(app); // 서버를 담음.
 const io = socketIO(server); // 서버를 담은 socketio를 담음.
 
-// server 실행 명령어
+// server basic settings
 app.use(express.static(path.join(__dirname, "src"))); // src의 내용을 서버로 열겠다.
 server.listen(PORT, ()=> console.log(`server is running... ${PORT}`));
 
-// io(chat.js)로 부터 데이터를 받음.
+// moving page
+
+
+//  get data from io(chat.js)
 io.on("connection",(socket)=>{
     socket.on("chatting", (data)=>{
         const {name, msg} = data;
--- chat/src/js/chat.js
+++ chat/src/js/chat.js
@@ -1,18 +1,14 @@
-"use strict" // js의 오류를 줄여나가는 방향
+"use strict" // js의 오류를 줄여나가는 방향의 일환?
 
-// socket io불러오기
-const socket = io();
-
+// Attribute
+const socket = io();// socket io불러오기
 const nickname = document.querySelector("#nickname");
 const chatList = document.querySelector(".chatting-list");
 const chatInput = document.querySelector(".chatting-input");
 const sendButton = document.querySelector(".send-button");
 const displayContainer = document.querySelector(".display-container");
 
-chatInput.addEventListener("keypress",(e)=>{
-    if(e.keyCode===13) send();
-});
-
+// send msg data to server
 function send(){
     const param = {
         name : nickname.value,
@@ -21,19 +17,16 @@ function send(){
     socket.emit("chatting", param); // 채널아이디, 내용 객체를 담아 소켓으로 보냄.
 }
 
-// event  - post msgs
-sendButton.addEventListener("click",send);
-
-//  get  msgs
+//  get  msgs from server
 socket.on("chatting",(data)=>{
     console.log(data);
     const {name, msg, time} = data;
     const item = new LiModel(name, msg, time);
     item.makeLi();
     displayContainer.scrollTo(0,displayContainer.scrollHeight);
-    
 })
 
+// Obj - li tag model about chatting block
 function LiModel(name, msg, time){
     this.name= name;
     this.msg = msg;
@@ -53,3 +46,9 @@ function LiModel(name, msg, time){
         chatList.appendChild(li);
     }
 }
+
+// Event
+sendButton.addEventListener("click",send);
+chatInput.addEventListener("keypress",(e)=>{
+    if(e.keyCode===13) send();
+});
