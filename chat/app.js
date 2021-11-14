e0c52d7078c50152b62cebfd02e94360254ae67c
portfolio_1st <aa991122aa@gmail.com>
2021-11-14 16:40:29 +0900

    code refactoring

--- chat/app.js
+++ chat/app.js
@@ -8,10 +8,8 @@ const express = require("express"); // node express를 사용할 수 있게 함.
 const http = require("http"); // socket io를 받을 수 있게 하기위함. +우린 웹소켓이니까 http를 사용
 const path = require("path") // 노드제이에스의 기본적인 모듈, url만들기 편리해짐
 const socketIO = require("socket.io"); // socket통신 모듈
-const moment = require("moment");
-const url = require('url');
-// const fs = require('fs');
-// const { render } = require("pug");
+const moment = require("moment"); // 시간 읽을수 있게하는 모듈
+const url = require('url');  // rest의 get으로 가져오기 위해 사용하는 모듈 (쿼리스트링분리용)
 
 // Constant
 const PORT = process.env.PORT || 5000;
@@ -28,12 +26,15 @@ app.set('view engine', 'pug'); // 원하는 엔진을 템플릿 엔진으로 사
 app.set('views', './src/views');	// view 파일들이 모여있는 폴더 지정
 
 // Variable
-let nickname ="";
+let nickname =""; // 전역으로 닉네임 관리
 
+// rendering chat.pug with nickname value;
 app.get('/chat', (req,res)=>{
+    // 쿼리스트링으로부터 분리
     let data = url.parse(req.url, true).query;
+    // 닉네임 저장
     nickname = data.nickname;
-    // res.redirect("/src/chat.html");
+    // chat.pug로 연결
     res.render('chat.pug', {nickname:nickname});
 });
 
@@ -43,10 +44,6 @@ io.on("connection",(socket)=>{
     socket.on("chatting", (data)=>{
         console.log("TEST");
         const {name, msg} = data;
-        io.emit("chatting",
-            {name,
-            msg,
-            time : moment(new Date()).format("h:mm A")
-        });
+        io.emit("chatting", {name, msg, time : moment(new Date()).format("h:mm A") });
     }); // 채팅아이디, 사용함수
 })
--- chat/src/js/chat.js
+++ chat/src/js/chat.js
@@ -2,6 +2,8 @@
 
 // Attribute
 const socket = io(); // socket io불러오기
+// const nickname = document.querySelector("#nickname"); 
+//  -> pug 쓰니까 처음 선언해둔건 변경후를 반영하지않아서 적용이 안됨. -> 쓸떄마다 불러서 사용함.
 const chatList = document.querySelector(".chatting-list");
 const chatInput = document.querySelector(".chatting-input");
 const sendButton = document.querySelector(".send-button");
@@ -13,13 +15,11 @@ function send(){
         name : document.querySelector("#nickname").innerText,
         msg : chatInput.value,
     }
-    console.log(param.name);
     socket.emit("chatting", param); // 채널아이디, 내용 객체를 담아 소켓으로 보냄.
 }
 
 //  get  msgs from server
 socket.on("chatting",(data)=>{
-    console.log(data);
     const {name, msg, time} = data;
     const item = new LiModel(name, msg, time);
     item.makeLi();
@@ -32,10 +32,15 @@ function LiModel(name, msg, time){
     this.msg = msg;
     this.time = time;
 
+    // make speech bubble
     this.makeLi = ()=>{
+        // get nickname value
         const nickname = document.querySelector("#nickname");
+        // make li tag
         const li = document.createElement("li");
+        // Identifies whether sent or received
         li.classList.add(nickname.innerText === this.name ? "sent": "receive");
+        // make dom value
         const dom = 
             `<span class='profile'>
             <span class="user">${this.name}</span>
@@ -43,13 +48,13 @@ function LiModel(name, msg, time){
             </span>
             <span class="message">${this.msg}</span>
             <span class="time">${this.time}</span>`;
+        // set dom value
         li.innerHTML = dom;    
+        // append li into chatlist
         chatList.appendChild(li);
     }
 }
 
 // Event
 sendButton.addEventListener("click",send);
-chatInput.addEventListener("keypress",(e)=>{
-    if(e.keyCode===13) send();
-});
+chatInput.addEventListener("keypress",(e)=>{ if(e.keyCode===13) send(); });
